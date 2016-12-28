5168f3297ad1d55838b1db7f4b0622137e808a19
nhareesha <hareesha1906@gmail.com>
2016-12-27 17:11:21 -0800

    Test commit - OCA 8 chapter-3 Java core apis - String manipulations

---
+++ src/main/java/com/notes/ocaprep/StringManipulations.java
@@ -0,0 +1,12 @@
+package com.notes.ocaprep;
+
+public class StringManipulations {
+
+	public static void main(String[] args) {
+		//string concat with any other object results in string
+		String s1 = "";
+		s1=s1+false;
+		
+	}
+
+}
