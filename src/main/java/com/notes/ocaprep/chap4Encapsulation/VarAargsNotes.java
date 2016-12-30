af1ae367519af20ad2004c99e195f37eabc4360a
nhareesha <hareesha1906@gmail.com>
2016-12-29 22:15:21 -0800

    Varargs notes

---
+++ src/main/java/com/notes/ocaprep/chap4Encapsulation/VarAargsNotes.java
@@ -0,0 +1,30 @@
+package com.notes.ocaprep.chap4Encapsulation;
+
+public class VarAargsNotes {
+
+	public static void main(String[] args) {
+		VarAargsNotes obj = new VarAargsNotes();
+		
+		//for vararg parameter we can pass an array or just values.
+		obj.test(2,3,4,5);// here 2 is 1st parameter and {3,4,5} forms the second parameter array
+		
+		//we cannot pass null .It throws NullPointerException
+		obj.test(1,null);
+		
+	}
+	
+	
+	public void test1(int... nums){}
+	
+	/* var args need to ve last in the argument list  */
+	public void test2(String str , int... nums){}
+	
+	/* DO NOT COMPILE . Because a method cannot have more than one varargs parameter */
+	
+	//public void test3(int... nums1 , int... nums2){}
+	
+	public void test(int n , int... nums){
+		System.out.println(nums.length);
+	}
+
+}
