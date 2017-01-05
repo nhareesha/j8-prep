d94bfd455edc90c49260bbd6cd509fa6341386c0
hareesha <hareesha1906@gmail.com>
2017-01-04 21:52:54 -0800

    updated

--- src/main/java/com/notes/ocaprep/chap4Encapsulation/DataEncapsulation.java
+++ src/main/java/com/notes/ocaprep/chap4Encapsulation/DataEncapsulation.java
@@ -39,11 +39,65 @@ public class DataEncapsulation {
 	 				}
 	 				
 	 				
-	 	4. 
+	 	4. Immutable class cannot be changed once after they are instantiated.
+	 	
+	 	5. For immutable classes , we want caller to be able to specify initial value - we just don't want 
+	 	   to change after the object is created.
+	 	   
+	 	6. For immutable classes we do not provide setters. Just the constructor that sets the property values.
+	 		which cannot be changed afterwards
 	 
 	 */
+	
+	
+	private boolean playing; // valid JavaBean convention to declare a property
+	
+	public int num; // Invalid  -- Java Beans cannot have public properties
+	
+	private String myName;
+	
+	/* valid JavaBean convention - boolean type properties getter begin with is*/
+	public boolean isPlaying(){return playing;}
+
+	/* Valid - Any setter begin with keyword set followed by upper case Variable letter*/
+	public void setPlaying(boolean playing) {this.playing = playing;}
+
+	/* InValid -- JavaBean Convention - getter should begin with keyword get*/
+	public int num() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+	
+	/* Valid*/
+	public String getMyName() {
+		return myName;
+	}
+
+	public void setMyName(String myName) {
+		this.myName = myName;
+	}
+
 	public static void main(String[] args) {
 
 	}
 
+}
+
+class Immutable{
+	
+	private String name;
+	
+	/*Immutable classes has public constructors*/
+	public Immutable(String name){
+		this.name = name;
+	}
+	
+	/* Immutable classes only allow getters*/
+	public String getName() {
+		return name;
+	}
+	
 }
