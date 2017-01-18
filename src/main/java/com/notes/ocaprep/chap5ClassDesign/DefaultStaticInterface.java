41897170d01b002155769c4c656bccdb31bd8a54
hareesha <hareesha1906@gmail.com>
2017-01-18 15:05:35 -0800

    updated

--- src/main/java/com/notes/ocaprep/chap5ClassDesign/DefaultStaticInterface.java
+++ src/main/java/com/notes/ocaprep/chap5ClassDesign/DefaultStaticInterface.java
@@ -1,11 +1,7 @@
-/**
- * 
- */
 package com.notes.ocaprep.chap5ClassDesign;
-
 /**
  * @author Hareesha
- * Jan 16, 2017 2017
+ * Jan 16, 2017
  */
 public class DefaultStaticInterface {
 
@@ -23,12 +19,42 @@ public class DefaultStaticInterface {
 	 		then all the classes would break and are forced to implement the added method.So
 	 		to avoid this a default method is put so that , which ever classes needs it can implement
 	 
+	 Rules for Default Methods:
 	 
-	 
+	 	1. A default method only be declared in Interface and not within any class or abstract class.
+	 	
+	 	2. A default method must be always be marked as default keyword.If the method is marked as
+	 		default, it must be provided with body.
+	 		
+	 	3. Default method is not assumed to be static, final or abstract, as it may be used or 
+	 		Overridden by a class that implements by a interface.
+	 		
+	 	4. Like all the methods in interface, default method is assumed to be public and will not compile
+	 		if it is marked as private or protected
+	 	
+	When interface extends another interface that has default method, 
+	
+		1. The interface that extends another interface may choose to ignore the default methods, 
+			in which case the default implementation for the method will be used.
+			
+		2. the interface may override the definition of default method using the standard rules 
+			for method overriding
+			
+		3. the interface may redeclare the default method as abstract, requiring classes that 
+			implements need to provide the implementation.		
 	 
 	 */
-	public static void main(String[] args) {
+	
+}
 
-	}
-
+interface Default{
+		//public default void firstDefault();//COMILER ERROR!! default method needs body
+	
+		//public  void firstDefault(){}//COMPILER ERROR!!non abstract method needs to be either static or default
+		
+		void firstDefault();//Valid!! public and abstract are assumed.
+		
+		default void secondDefault(){
+			System.out.println("");
+		}
 }
