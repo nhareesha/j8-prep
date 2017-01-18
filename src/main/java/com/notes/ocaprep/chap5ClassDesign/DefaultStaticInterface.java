59357872d9ce2bfa62303298dc979ab59065587f
hareesha <hareesha1906@gmail.com>
2017-01-17 23:14:19 -0800

    deafultmethods of Interface

---
+++ src/main/java/com/notes/ocaprep/chap5ClassDesign/DefaultStaticInterface.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.notes.ocaprep.chap5ClassDesign;
+
+/**
+ * @author Hareesha
+ * Jan 16, 2017 2017
+ */
+public class DefaultStaticInterface {
+
+	/**
+	 Default Methods:
+	 	
+	 	1. A default method is a method defined within an interface with default keyword
+	 		in which a method body is provided.
+	 	 
+	 	2. Default method within an interface defines a method with a default implementation.
+	 	
+	 	3. Default methods are introduced in java 8 to help with code development and backward compatibility.
+	 	
+	 	4. If an interface is implemented by n number of classes and there is any update to interface
+	 		then all the classes would break and are forced to implement the added method.So
+	 		to avoid this a default method is put so that , which ever classes needs it can implement
+	 
+	 
+	 
+	 
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
