16779378d5d204bca5c7d3b93392e3155906a5ce
hareesha <hareesha1906@gmail.com>
2017-01-23 13:43:11 -0800

    Polumorphism notes

---
+++ src/main/java/com/notes/ocaprep/chap5ClassDesign/Polymorphism.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package com.notes.ocaprep.chap5ClassDesign;
+
+/**
+ * @author Hareesha
+ * Jan 23, 2017 2017
+ */
+public class Polymorphism {
+
+	/**
+	Polymorphism:
+		1. A java object may be accessed using a reference with same type as object, a reference 
+			that is super class of object, or a reference that defines an interface the object 
+			implements,either directly or through a superclass.
+			
+		2. Once object has been assigned to a new reference type, only the methods and variables
+			available on the reference type are called on the object without an explict cast
+			
+			Eg: Object obj = new String();
+				obj is the reference
+				Object is the reference type
+				new String() is actual object
+				
+		3. In java all objects are accessed by reference, as a developer we never have direct access
+			to the object itself
+			
+		4. 
+	
+	
+	
+	
+	
+	
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
