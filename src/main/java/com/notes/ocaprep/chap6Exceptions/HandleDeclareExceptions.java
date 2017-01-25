0ebb512209c6472eee9c10cc1227213835800aa4
hareesha <hareesha1906@gmail.com>
2017-01-24 23:20:27 -0800

    Calling a method that throw Exception

--- src/main/java/com/notes/ocaprep/chap6Exceptions/HandleDeclareExceptions.java
+++ src/main/java/com/notes/ocaprep/chap6Exceptions/HandleDeclareExceptions.java
@@ -1,6 +1,9 @@
 
 package com.notes.ocaprep.chap6Exceptions;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 /**
  * @author Hareesha
  * Jan 24, 2017
@@ -34,10 +37,79 @@ public class HandleDeclareExceptions {
 	 	
 	 10. At any point of time only one catch block is executed if an Exception arises.
 	 
+	 11. If we are calling a method throws a checked Exception we are required to handle or declare
+	 	that Exception the calling method.This rule applies only to CHECKED EXCEPTIONS.
+	 	
+	 12. The method that overrides the super class methods that "throws" Checked Exceptions, must throw 
+	 	fewer exceptions or no new Checked Exceptions in the overridden method.
+	 	This applies only to CHECKED EXCEPTIONS.
+	 
 	 
 	 */
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws IOException {
+		
+		HandleDeclareExceptions obj=new HandleDeclareExceptions();
+		
+		//obj.methodOne();
+		// COMPILER ERROR!! as the method throws Exception it required caller to declare or handle
+					
+		obj.methodOne();
+		
+		obj.methodTwo();
+		//VALID!! Although it throws RuntimeException, they are not required to be handled
+		
+		try{
+			obj.methodOne();
+		}catch(IllegalArgumentException e){
+			// ITS Okay to catch RuntimeException, even if one is not thrown
+		}
+		
+		try{
+			obj.methodThree();
+		}catch(IOException e){}
+		//catch (FileNotFoundException e) {} 
+		//Invalid because it is sub class of IOException and it is unreachable
+		
+		
+		try{
+			obj.methodTwo();
+		}
+		//catch(IOException e){}
+		//Invalid because Compiler know that , methodTwo cannot throw a IOException as it is not declared
+		
+		catch(RuntimeException e){}
+		
+		//catch(IllegalArgumentException e){}
+		//Invalid because , it is subclass of RuntimeException, which has already been declared
+		
+		
+		try{
+			System.out.println("Try-block");
+			throw new Exception();
+		}catch(Exception e){
+			System.out.println("Catch-block");
+			throw new IOException("I am a Exception in Catch block");
+			//this is never printed, as it is forgotten by compiler because finally has thrown an 
+			//Exception , which ultimately will be printed
+		}finally{
+			System.out.println("Fnillay is always run no matter what unless it is System.exit(0)");
+			throw new NullPointerException("I am a null pointer. And I am masking Catch block Exception");
+		}
+		
+		
 	}
+	
+	public void methodOne()throws IOException{}
+	
+	public void methodTwo() throws IllegalArgumentException{}
+	
+	public void methodThree() throws IOException{}
+}
 
+class ExceptionTest extends HandleDeclareExceptions{
+	
+	public void methodTwo() throws NumberFormatException{} //Valid can declare any RuntimeExceptions
+	
+	//public void methodThree() throws Throwable{} 
+	// Overridden method must not throw a broader Checked Exceptions
 }
