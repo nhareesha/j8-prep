d49fd2c1925b456264d2cbd711e481cadea420a9
hareesha <hareesha1906@gmail.com>
2017-01-26 12:25:59 -0800

    update

--- src/main/java/com/notes/ocaprep/chap1JavaBuildingBlocks/VariablesNotes.java
+++ src/main/java/com/notes/ocaprep/chap1JavaBuildingBlocks/VariablesNotes.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package com.notes.ocaprep.chap1JavaBuildingBlocks;
 
 /**
@@ -10,6 +7,7 @@ package com.notes.ocaprep.chap1JavaBuildingBlocks;
 public class VariablesNotes {
 
 	/**
+	 
 	 Variables:
 	 	1. Variable is a piece of name that stores the data in the memory.
 	 	
@@ -35,15 +33,70 @@ public class VariablesNotes {
 	 	
 	 	3. We cannot use java reserved words for identifiers.
 	 	
-	 Default Initialization Of Variables:
+	 						Default Initialization Of Variables:
 	 
-	 	1. 
-	 
+	 	Local Variables
+	 	
+	 	 1. Local variables must be initialized before use.
+	 	 
+	 	 2. They do not have default value ,until initialized they they hold garbage value.
+	 	 
+	 	 3. The compiler will not let you read a uninitialized value.
+	 	 
+	 	 4. Local variables cannot have larger scope than the method they are defined in
+	 	 
+	 	Instance and Class(static) Variables:
+	 	
+	 	1. Instance and Static variables do not require you to initialize them.
+	 	
+	 	2. Inastance and class variables get the default values as soon as they are declared.
+	 	
+	 	3. Default Values:
+	 		boolean - false
+	 		
+	 		byte, short, int ,long - 0(in the type's bit length)
+	 		
+	 		float, double - 0.0
+	 		
+	 		char - '\u0000'(NULL)
+	 		
+	 		any object - null
+	 		
+	 	4. Ordering of element in file PIC[ Package , Import , Class ]
+	 	 	
 	 */
 	
+	String a , b; //VALID
+	
+	String c = "hello" , d;
+	
+//	String e , String f; multiple varaibles on statement should share same type
+	
+	int i ; int j;//valid ; separes the statements
+	
+	 // int k , String c; multiple varaibles declarations of different types cannot be in ne statement
+	
+	boolean $h;
+	
+	boolean _h;
+	
+	//boolean h@;//@ is invalid character
+	
+	//boolean char;//reserved words are not allowed
+	
+	boolean Char;//Valid , java is case sensitive
+	
+	//boolean 9h; numbers are not allowed in the first place
+	
+	static char var1;
 	
 	public static void main(String[] args) {
-
+		
+		char var2;
+		
+	// System.out.println(var2);//local variable cannot be used without initialization
+		
+		System.out.println(var1);
 	}
 
 }
