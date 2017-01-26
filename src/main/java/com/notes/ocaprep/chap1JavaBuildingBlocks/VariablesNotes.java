c4a088463b7435d45abe3b12b9646fde7be387a0
hareesha <hareesha1906@gmail.com>
2017-01-26 00:28:13 -0800

    variables

---
+++ src/main/java/com/notes/ocaprep/chap1JavaBuildingBlocks/VariablesNotes.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package com.notes.ocaprep.chap1JavaBuildingBlocks;
+
+/**
+ * @author Hareesha
+ * Jan 25, 2017 2017
+ */
+public class VariablesNotes {
+
+	/**
+	 Variables:
+	 	1. Variable is a piece of name that stores the data in the memory.
+	 	
+	 Multiple Variables in same statement:
+	 
+	 	1. We can declare and initialize multiple variables in same line as long they are of same 
+	 		data type.
+	 		
+	 		Ex : int i,j;
+	 			 int i=0,j;
+	 			 int i , j=20;
+	 			 
+	 		
+	 	2. Multiple variable declarations should be separated by  a comma and should have same type.
+	 	
+	 	3. Semicolon separates statements in java.
+	 	
+	 Identifier Naming Rules:
+	 
+	 	1. Name must begin with letter or symbol $ _
+	 	
+	 	2. Subsequent characters after the first letter can be numbers, letters, $ _
+	 	
+	 	3. We cannot use java reserved words for identifiers.
+	 	
+	 Default Initialization Of Variables:
+	 
+	 	1. 
+	 
+	 */
+	
+	
+	public static void main(String[] args) {
+
+	}
+
+}
