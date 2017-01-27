3fdcc2673ecb39def09c3dd4ec924b8879cc3b83
hareesha <hareesha1906@gmail.com>
2017-01-27 11:17:27 -0800

    Number systems - octal , hexadecimal , binary,decimal

---
+++ src/main/java/com/notes/ocaprep/chap1JavaBuildingBlocks/NumberSystems.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.notes.ocaprep.chap1JavaBuildingBlocks;
+
+/**
+ * @author Hareesha
+ * Jan 27, 2017 2017
+ */
+public class NumberSystems {
+
+	/**
+	 NumberSystems:
+	 	
+	 	1. Octal - [base 8]-[0- 7] - [Prefix : 0]- [Ex : 017 - Decimal eq : 8+7 = 15]
+	 	
+	 	2. Hexadecimal -[base 16]-[0-9 A-F] -[Prefix : 0x 0X] -[Ex : 0xA3 - Decimal eq: (16*10)+3 =163]
+	 	
+	 	3. Binary-[base 2]-[0 1]-[Prefix : 0b or 0B] - [Ex: 0b10]-[Ex: (2*1)+0 = 2]
+	
+		4. decimal -[base-10]-[0-9]
+	
+	 */
+	public static void main(String[] args) {
+		
+		//Octal rep
+		int a = 076;//decimal - 56+6 =62
+		
+		int b = 0XA4;//decimal - 160+4 = 164
+		int c = 0b11;//decimal - 3
+
+	}
+
+}
